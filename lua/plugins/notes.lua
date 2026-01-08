fc567000af233cc066fbe4a5176b7f6ef90ea228
Chyle Andrei Lee <chyleandrei.lee@emerson.com>
2026-01-08 22:17:12 +0800

    Add additional configs for obsidian

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -3,8 +3,30 @@ return {
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
   },
   opts = {
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
     workspaces = {
       {
         name = "personal",
@@ -15,8 +37,35 @@ return {
         path = "~/vaults/work",
       },
       {
+        name = "Zettelkasten",
+        path = "~/vaults/zettelkasten",
+      },
+      {
         name = "archive",
         path = "~/vaults/archive",
+      },
+    },
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
       },
     },
     templates = {
